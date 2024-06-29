eb76ccc8bc2a17ddbe3010773549632692874b75
Icelycoool <mohammedcali@proton.me>
2024-06-29 16:15:29 -0700

    Corrected the check

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,17 @@
 #!/usr/bin/pup
 # Installs flask from pip3
 
+package {'python3.8':
+  ensure   => '3.8.10',
+  provider => 'pip3',
+}
+
 package {'flask':
   ensure   => '2.1.0',
-  provider => 'pip3'
+  provider => 'pip3',
+}
+
+package {'werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
 }
