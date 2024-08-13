0021e05be5274d10593c15e469368dddaaf929a2
Icelycoool <mohammedcali@proton.me>
2024-08-14 02:11:35 +0300

    Task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,13 +8,13 @@ def number_of_subscribers(subreddit):
     Queries the Reddit API and returns the number
     of subscribers to the subreddit.
     """
-
     req = requests.get(
         "https://www.reddit.com/r/{}/about.json".format(subreddit),
         headers={"User-Agent": "Custom"},
+        allow_redirects=False
     )
 
     if req.status_code == 200:
-        return req.json().get("data").get("subscribers")
+        return req.json().get("data", {}).get("subscribers", 0)
     else:
         return 0
