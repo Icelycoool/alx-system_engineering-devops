c0ff35a61b46649726feb606820689b5667554b8
Icelycoool <mohammedcali@proton.me>
2024-08-14 02:15:23 +0300

    Task 0 Final Commit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,21 +1,17 @@
 #!/usr/bin/python3
-"""
-Function that queries the Reddit API and returns the number of subscribers
-(not active users, total subscribers) for a given subreddit.
-If an invalid subreddit is given, the function should return 0
-"""
-
+"""Module for Task 0"""
 import requests
 
 
 def number_of_subscribers(subreddit):
     """
-    Function that queries the Reddit API
-    - If not a valid subreddit, return 0.
+    Queries the Reddit API and returns the number
+    of subscribers to the subreddit.
     """
     req = requests.get(
         "https://www.reddit.com/r/{}/about.json".format(subreddit),
         headers={"User-Agent": "Custom"},
+        allow_redirects=False
     )
 
     if req.status_code == 200:
