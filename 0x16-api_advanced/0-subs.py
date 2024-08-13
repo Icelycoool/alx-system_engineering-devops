5e0b8a1277bd880f8ad0a831a32c179ca99bfd32
Icelycoool <mohammedcali@proton.me>
2024-08-14 02:32:26 +0300

    Task 0 checks

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -12,7 +12,7 @@ def number_of_subscribers(subreddit):
         "https://www.reddit.com/r/{}/about.json".format(subreddit),
         headers={"User-Agent": "Custom"},
     )
-
+    
     if req.status_code == 200:
         return req.json().get("data").get("subscribers")
     else:
