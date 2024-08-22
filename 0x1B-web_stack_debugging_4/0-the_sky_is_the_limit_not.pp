43880443d37c359c1240f323d6f4fc75669899de
Icelycoool <mohammedcali@proton.me>
2024-08-22 16:00:19 -0700

    Task 0 Final

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -5,7 +5,7 @@ exec { 'fix-nginx':
     command => '/bin/sed -i "s/15/4096/" /etc/default/nginx',
     path    => '/usr/local/bin/:/bin/',
 }
-->
+
 # Restart nginx
 exec { 'nginx-restart':
     command => '/etc/init.d/nginx restart',
