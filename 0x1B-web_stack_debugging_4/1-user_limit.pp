966cdfbdb12bd3eaaa41da7c531b7ab2be4ddec1
Icelycoool <mohammedcali@proton.me>
2024-08-22 16:17:18 -0700

    Task 1

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,13 @@
+# Enable the user holberton without error
+
+# Increase hard file limit for Holberton user
+exec { 'hard-file-limit-for-Holberton':
+    command => 'sed -i "/^holberton hard/s/5/50000" /etc/security/limits.conf',
+    path    => '/usr/local/bin/:/bin/'
+}
+
+# Increase soft file limit for Holberton
+exec { 'soft-file-limit-for-Holberton':
+    command => 'sed -i "/^holberton soft/s/4/50000/" /etc/security/limits.conf',
+    path    => 'usr/local/bin/:/bin/'
+}
