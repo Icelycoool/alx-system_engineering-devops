9b2757a266e4cc2896320069187eb81e3f39b862
Icelycoool <mohammedcali@proton.me>
2024-08-22 15:33:32 -0700

    Task 0

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -2,12 +2,12 @@
 
 # Increase teh ulimit
 exec { 'fix-nginx':
-	command  =>  'bin/sed -i "n/15/4096/" /etc/default/nginx',
-	path     =>  '/usr/local/bin/:/bin/',
+    command => 'bin/sed -i "n/15/4096/" /etc/default/nginx',
+    path    => '/usr/local/bin/:/bin/',
 }
 
 # Restart nginx
 exec { 'nginx-restart':
-	command  =>  '/etc/init.d/nginx restart',
-	path     =>  '/etc/init.d/',
+    command => '/etc/init.d/nginx restart',
+    path    => '/etc/init.d/',
 }
